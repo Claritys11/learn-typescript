bebfebd473630b41ad1fd6f212fe1afb1e2481bd
Claritys <elang_syadewa_34rpl@student.smktel-mlg.sch.id>
2026-07-28 15:16:26 +0700

    Nyicil dikit dikit hehe

--- howtogit.md
+++ howtogit.md
@@ -1,6 +1,9 @@
 2. Cara Kerja SelanjutnyaJika ingin mengambil update terbaru dari zakaria29:bash
-git pull upstream main / git fetch upstream
+git pull upstream main (jangan pake ini deh) /
+git fetch upstream 
 setelah itu 
-git merge origin/main
+git merge upstream/main
 Jika ingin menyimpan hasil kerja barumu ke Claritys11:bash
+git add .
+git commit -m "Terserah"
 git push origin main
--- src/02-operators/tasks/task-01.ts
+++ src/02-operators/tasks/task-01.ts
@@ -1,3 +1,5 @@
+import { fromWritable } from "node:stream/iter";
+
 /**
  * The school cafeteria sells lunch packages to students. Today, Raka purchased 3 fried rice meals, each costing Rp18,000, and 2 bottles of mineral water, each costing Rp5,000. Because he is a member of the student council, he received a Rp10,000 discount.
  * The cashier wants to calculate:
@@ -14,3 +16,41 @@
  *  - Final payment
  * 2. Display the calculation results.
  */
+type Quantity = {
+    friedRice: number
+    drinks: number
+    member: boolean
+} 
+type Packages = {
+    friedRice: number
+    drinks: number
+} 
+let Packages: Packages = {
+    friedRice: 18000,
+    drinks: 5000
+}
+let Raka: Quantity = {
+    friedRice: 3,
+    drinks: 2,
+    member: true
+}
+function countFood(): number {
+    return Packages.friedRice * Raka.friedRice
+}
+function countDrinks(): number {
+    return Packages.drinks * Raka.drinks
+}
+function countGrandTotal(): number {
+    return Packages.friedRice * Raka.friedRice + Packages.drinks * Raka.drinks
+}
+function countTotal(): number {
+    if(Raka.member == true){
+        return (Packages.friedRice * Raka.friedRice + Packages.drinks * Raka.drinks) - 10000;
+    } else {
+        return Packages.friedRice * Raka.friedRice + Packages.drinks * Raka.drinks;
+    }
+}
+console.log(`Total biaya makanan: ${countFood()}`)
+console.log(`Total biaya minuman: ${countDrinks()}`)
+console.log(`Total biaya sebelum diskon (jika ada): ${countGrandTotal()}`)
+console.log(`Total biaya sesudah diskon (jika ada): ${countTotal()}`)
